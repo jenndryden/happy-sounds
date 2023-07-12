cfcda0f0dcd3b62b42adebd88103815a4d370a58
Jenn Dryden <jennifer.dryden@uwaterloo.ca>
2023-07-11 20:20:35 -0400

    :lipstick: adding button to start and modifying sliders

--- happy-sounds/src/App.js
+++ happy-sounds/src/App.js
@@ -1,17 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import logo from './logo.png';
+import name from './jenn.png';
+import rain from './rain.mp3';
+import brownnoise from './brownnoise.mp3';
+import leaves from './leaves.mp3';
+import laughter from './laughter.mp3';
+import waves from './waves.mp3';
+import bonfire from './bonfire.mp3';
 import AudioPlayer from 'react-audio-player';
+import rainlogo from './audionames/rain.png';
+import laughterlogo from './audionames/laughter.png';
+import leaveslogo from './audionames/leaves.png';
+import waveslogo from './audionames/waves.png';
+import brownnoiselogo from './audionames/brownnoise.png';
+import bonfirelogo from './audionames/bonfire.png';
+
+const sliderStyle = {
+  WebkitAppearance: 'none',
+  background: 'white',
+  height:'10px',
+  width: '90%',
+  maxWidth: '500px',
+  outline: 'none',
+  borderRadius: '25px',
+};
 
 const AudioSliderApp = () => {
   const [volumes, setVolumes] = useState({});
   const [audioFiles, setAudioFiles] = useState([
-    { id: 1, volume: 0, src: '/rain.mp3' },
-    { id: 2, volume: 0, src: '/brownnoise.mp3' },
-    { id: 3, volume: 0, src: '/rain.mp3'},
-    { id: 4, volume: 0, src: '/rain.mp3' },
-    { id: 5, volume: 0, src: '/rain.mp3' },
-    { id: 6, volume: 0, src: '/rain.mp3'},
+    { id: 1, volume: 0, src: rain, imgSrc: rainlogo },
+    { id: 2, volume: 0, src: brownnoise, imgSrc: brownnoiselogo },
+    { id: 3, volume: 0, src: laughter, imgSrc: laughterlogo},
+    { id: 4, volume: 0, src: waves, imgSrc: waveslogo },
+    { id: 5, volume: 0, src: bonfire, imgSrc: bonfirelogo },
+    { id: 6, volume: 0, src: leaves, imgSrc: leaveslogo },
   ]);
 
   const handleSliderChange = (id, volume) => {
@@ -26,11 +49,22 @@ const AudioSliderApp = () => {
     }));
   };
 
+  const [sliderValue, setSliderValue] = useState(0);
+  const [isStarted, setIsStarted] = useState(false);
+
+const startAudio = () => {
+    setIsStarted(true);
+};
+
+
   return (
     <div className="container">
       <img src={logo} alt="Logo" className="logo" />
+      <img src={name} alt="Jenn" className="name" />
       <div className="circle">
-        {audioFiles.map((audioFile, index) => {
+      {!isStarted && <div className="sprite-button" onClick={startAudio}>
+  </div>}
+        {isStarted && audioFiles.map((audioFile, index) => {
           const angle = (index * 360) / audioFiles.length;
           let positionX = 0;
           let positionY = 0;
@@ -47,27 +81,36 @@ const AudioSliderApp = () => {
           
             return (
               <div key={audioFile.id} className="slider" style={{ transform }}>
-                <p>Audio File {audioFile.id}</p>
+                <img src={audioFile.imgSrc} alt={audioFile.imgSrc} className="audiologo" />
                 <input
                   type="range"
                   min="0"
                   max="100"
                   value={volume}
                   onChange={(e) => handleSliderChange(audioFile.id, e.target.value)}
+                  onInput={(e) => {
+                    const value = Number(e.target.value) / 100;
+                    e.target.style.setProperty('--thumb-rotate', `${value * 720}deg`);
+                    setSliderValue(Math.round(value * 50));
+                  }}
+                  className="input-thumb"
+                  style={sliderStyle} 
                 />
                 <br />
+                
                 <AudioPlayer
                   src={audioFile.src}
                   volume={volume / 100}
-                  autoPlay={true}
-                  controls={true}
+                  autoPlay
+                  loop={true}
+                  controls={false}
                 />
               </div>
             );
           })}
+        </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default AudioSliderApp;
